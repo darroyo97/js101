82526226b83e6d602247fc03355a5c3cd4fc4bca
Daniela <daniela.arroyo27@gmail.com>
2020-02-04 14:56:19 -0600

    level 1 complete

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,51 @@
-function lvl1exercise1 () {
+function lvl1exercise1() {
   // Declare a variable without instantiating it and return it!
+  var myName;
 
 }
 
-function lvl1exercise2 () {
+function lvl1exercise2(num) {
   // Declare and instantiate a number and return it
+  var num = 27;
+  return num
+
 
 }
 
-function lvl1exercise3 () {
+function lvl1exercise3(float) {
   // Declare and instantiate a floating point number that is not a whole number and return it
+  var float = 3.7
+  return float
 
 }
 
-function lvl1exercise4 () {
+function lvl1exercise4(myString) {
   // Declare and instantiate a string "Hello World!" and return it
+  var myString = "Hello World!"
+  return myString
 
 }
 
-function lvl1exercise5 () {
+function lvl1exercise5(myArray) {
   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  var myArray = ["Hello World!", 4]
+  return myArray
 
 }
 
-function lvl1exercise6 () {
+function lvl1exercise6(objName) {
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+  var objName = {
+    key1: "Hello World!",
+    key2: 4
+  }
+  return objName
 
 }
 
-function lvl1exercise7 () {
+function lvl1exercise7(bull) {
   // Declare and instantiate a boolean value 'false' and return it
+  var bull = false
+  return bull
 
 }
