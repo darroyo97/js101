7582146028b4fb9d609def2d997e856fda20901c
Daniela <daniela.arroyo27@gmail.com>
2020-02-05 09:58:33 -0600

    level3 completed

--- exercises/level3.js
+++ exercises/level3.js
@@ -8,20 +8,32 @@ function lvl3exercise1() {
 
 function lvl3exercise2() {
   // Create a "hello" and a 12, return the concatenation of the two
+  var concat = "hello" + 12
+  return concat
 
 }
 
 function lvl3exercise3() {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
+  var num = 12;
+  var n = num.toString();
+  return n
 
 }
 
 function lvl3exercise4() {
   // Create a "hello world!" string. Return the length of the string
+  var string1 = "hello world!"
+  var lstring1 = string1.length;
+  return lstring1
 
 }
 
 function lvl3exercise5() {
   // Create a "hello world!" string. Return the index of the word "world".
+  var string1 = "hello world!";
+  var indexString1 = string1.indexOf('world');
+  return indexString1
+
 
 }
