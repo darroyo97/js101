49753f721757413a2bd9ee3c74f3818911357303
Daniela <daniela.arroyo27@gmail.com>
2020-02-05 10:10:49 -0600

    level 4 working on it

--- exercises/level4.js
+++ exercises/level4.js
@@ -1,29 +1,33 @@
-function lvl4exercise1 () {
+function lvl4exercise1() {
   // Return the boolean value "true"
-
+  var bool = true
+  return bool
 }
 
-function lvl4exercise2 () {
+function lvl4exercise2() {
   // Return the boolean value "false"
+  var bool = false
+  return bool
 
 }
 
-function lvl4exercise3 (bool) {
+function lvl4exercise3(bool) {
   // Return the opposite of the input boolean value
-
+  bool = true
+  return !bool
 }
 
-function lvl4exercise4 (bool1, bool2) {
+function lvl4exercise4(bool1, bool2) {
   // Return the logical "and" of the input boolean values
 
 }
 
-function lvl4exercise5 (bool1, bool2) {
+function lvl4exercise5(bool1, bool2) {
   // Return the logical "or" of the input boolean values
 
 }
 
-function lvl4exercise6 (bool1, bool2) {
+function lvl4exercise6(bool1, bool2) {
   // Return the logical "equivalence" of the input boolean values
 
 }
