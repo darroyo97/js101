9cd83ba3d581727e54a4aef366a9423f209fc448
Daniela <daniela.arroyo27@gmail.com>
2020-02-05 09:46:32 -0600

    level 2 complete

--- exercises/level2.js
+++ exercises/level2.js
@@ -1,3 +1,4 @@
+
 function lvl2exercise1(num1, num2) {
   // Return the sum of num1 and num2
   total = num1 + num2
@@ -13,15 +14,20 @@ function lvl2exercise2(num1, num2) {
 
 function lvl2exercise3(num1, num2) {
   // Return the multiplication of num1 and num 2
-
+  total = num1 * num2
+  return total
 }
 
 function lvl2exercise4(num1, num2) {
   // Return the division of num1 and num2
+  total = num1 / num2
+  return total
 
 }
 
 function lvl2exercise5(num1) {
   // Add 2 to num1 using += and return it
+  num1 += 2
+  return num1
 
 }
--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,27 @@
-function lvl3exercise1 () {
+function lvl3exercise1() {
   // Create a "hello" and a "world", return the concatenation of the two
-
+  var string1 = "hello"
+  var string2 = "world"
+  var concat = string1 + string2
+  return concat
 }
 
-function lvl3exercise2 () {
+function lvl3exercise2() {
   // Create a "hello" and a 12, return the concatenation of the two
 
 }
 
-function lvl3exercise3 () {
+function lvl3exercise3() {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
 
 }
 
-function lvl3exercise4 () {
+function lvl3exercise4() {
   // Create a "hello world!" string. Return the length of the string
 
 }
 
-function lvl3exercise5 () {
+function lvl3exercise5() {
   // Create a "hello world!" string. Return the index of the word "world".
 
 }
